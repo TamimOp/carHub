e362f30e395150591e3eb11e2c9703e33db8a1d8
TamimOp <tamimshadanik@gmail.com>
2024-09-02 23:41:12 +0600

    navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -18,10 +18,16 @@ const Hero = () => {
           process.
         </p>
         <CustomButton
-          title="Explore Car"
+          title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/hero.png" alt="hero" fill className="object-contain" />
+          <div className="hero__image-overlay"></div>
+        </div>
       </div>
     </div>
   );
