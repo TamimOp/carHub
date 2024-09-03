d99af9e8dae25d46861fa0dcff9dd65e2da8cc4c
TamimOp <tamimshadanik@gmail.com>
2024-09-03 22:22:31 +0600

    footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 const Footer = () => {
-  return <div></div>;
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <Image
+            src="/logo.svg"
+            alt="logo footer"
+            width={118}
+            height={18}
+            className="object-contain"
+          />
+          <p className="text-base text-gray-700">
+            Carhub 2024 <br />
+            All right reserved ©
+          </p>
+        </div>
+        <div className="footer__links"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
